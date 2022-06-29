44b8dafb730493748cbfbf43a823035293ac6d25
ik <doitdoik@naver.com>
2022-06-29 14:55:09 +0900

    boj2702

---
+++ brute_force/boj_2702.py
@@ -0,0 +1,17 @@
+import sys
+
+def lcm(a, b) :
+  return int((a*b) / gcd(a, b))
+
+def gcd(a, b) :
+  if a == 0 :
+    return b
+  else :
+    return gcd(b % a, a)
+
+input = sys.stdin.readline
+t = int(input())
+
+for i in range(t) :
+    a, b = map(int, input().split())
+    print(lcm(a, b), gcd(a, b))
