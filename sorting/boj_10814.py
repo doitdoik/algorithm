ae674e008ba241c9273e361db1d55ad4d55ceae4
ik <doitdoik@naver.com>
2022-07-19 15:31:19 +0900

    boj_10814

---
+++ sorting/boj_10814.py
@@ -0,0 +1,13 @@
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+arr = []
+for _ in range(n) :
+    arr.append(list(input().split()))
+
+arr.sort(key = lambda x : int(x[0]))
+
+for i in arr :
+    print(i[0], i[1])
+
