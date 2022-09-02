737d60f0be9abd2cc2a49152b6802c119c69f86b
ik <doitdoik@naver.com>
2022-09-02 17:08:05 +0900

    boj2512

---
+++ binary_search/boj_2512.py
@@ -0,0 +1,25 @@
+n = int(input())
+arr = list(map(int, input().split()))
+m = int(input())
+
+def budget(arr):
+    start = 0
+    end = max(arr)
+
+    while start <= end :
+        mid = (start + end) // 2
+        cnt = 0
+
+        for i in range(n) :
+            if arr[i] >= mid :
+                cnt += mid
+            else :
+                cnt += arr[i]
+
+        if cnt <= m :
+            start = mid + 1
+        else :
+            end = mid - 1
+    return end
+
+print(budget(arr))
