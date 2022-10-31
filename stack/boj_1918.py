eec32dfba17457bf90d8286811ae62c36e6c2cd2
ik <doitdoik@naver.com>
2022-10-31 13:48:49 +0900

    boj1918

---
+++ stack/boj_1918.py
@@ -0,0 +1,29 @@
+import sys
+
+input = sys.stdin.readline
+ex = list(input())
+stack = []
+res = ''
+
+for s in ex:
+    if s.isalpha():
+        res += s
+    else:
+        if s == '(':
+            stack.append(s)
+        elif s == '*' or s == '/':
+            while stack and (stack[-1] == '*' or stack[-1] == '/'):
+                res += stack.pop()
+            stack.append(s)
+        elif s == '+' or s == '-':
+            while stack and (stack[-1] != '('):
+                res += stack.pop()
+            stack.append(s)
+        elif s == ')':
+            while stack and stack[-1] != '(':
+                res += stack.pop()
+            stack.pop()
+while stack:
+    res += stack.pop()
+
+print(res)
