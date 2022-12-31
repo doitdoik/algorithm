5d37306aa8f709c94251ec327ceb1b74d0463716
ik <doitdoik@naver.com>
2022-12-31 13:06:31 +0900

    boj5988

---
+++ string/boj_5988.py
@@ -0,0 +1,12 @@
+import sys
+
+input=sys.stdin.readline
+n = int(input())
+
+for _ in range(n) :
+    tmp = int(input())
+
+    if tmp % 2 == 0 : 
+        print("even")
+    else : 
+        print("odd")
