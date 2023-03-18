4685674afba73b1a0fca864aa4e354d57304de41
ik <doitdoik@naver.com>
2023-03-19 00:12:53 +0900

    boj5543

---
+++ mathematics/boj_5543.py
@@ -0,0 +1,12 @@
+# boj 5543 상근날드
+
+burger = []
+for _ in range(3):  
+    burger.append(int(input()))
+    
+drink = []
+for _ in range(2):  
+    drink.append(int(input()))
+
+set = min(burger) + min(drink) - 50  
+print(set)
