15ab6543ccd5da3e05af88c3a26b2cf17c352368
ik <doitdoik@naver.com>
2023-05-03 00:13:19 +0900

    boj14490

---
+++ string/boj_14490.py
@@ -0,0 +1,9 @@
+# boj 14490 백대열
+def gcd(a, b):
+    if b == 0:
+        return a
+    return gcd(b, a%b)
+
+a, b = map(int, input().split(':'))
+t = gcd(max(a, b), min(a, b))
+print(f"{a//t}:{b//t}")
