8cc3dbf7678dd1f19b44da53518c8ea3e37a2f0d
ik <doitdoik@naver.com>
2023-05-07 00:32:33 +0900

    boj12605

---
+++ string/boj_12605.py
@@ -0,0 +1,8 @@
+# boj 12605 단어순서 뒤집기
+import sys
+input = sys.stdin.readline
+n = int(input())
+
+for i in range(1, n+1):
+    words = list(input().rstrip().split())
+    print('Case #%d: %s' %(i, ' '.join(words[::-1])))
