4e6335ce27c9640cadb9f217ed905db57fc6cd83
ik <doitdoik@naver.com>
2023-06-04 11:35:23 +0900

    boj5063

---
+++ mathematics/boj_5063.py
@@ -0,0 +1,12 @@
+# boj 5063 TGN
+t = int(input())
+
+for i in range(t):
+    a, b, c = map(int, input().split())
+    d = b - c
+    if a > d:
+        print("do not advertise")
+    elif a < d:
+        print("advertise")
+    else:
+        print("does not matter")
