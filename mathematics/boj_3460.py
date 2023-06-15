65ac71520809838054e39f1846b8b11d3dc60a6c
ik <doitdoik@naver.com>
2023-06-15 17:48:29 +0900

    boj3460

--- mathematics/boj_3460.py
+++ mathematics/boj_3460.py
@@ -1,12 +1,9 @@
 # boj 3460 이진수
 
 t = int(input())
- 
+    
 for _ in range(t):
     n = bin(int(input()))[2:]
     for i in range(len(n)):
         if n[-i - 1] == '1':
             print(i, end=' ')
-
-    
-        
