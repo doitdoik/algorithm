05b2d13d83be1f4ae95e19bcfd722d320ea039ce
ik <doitdoik@naver.com>
2023-07-08 20:32:15 +0900

    boj10103

---
+++ mathematics/boj_10103.py
@@ -0,0 +1,12 @@
+# boj 10103 주사위 게임
+n = int(input())
+x = 100
+y = 100
+for i in range(n):
+    a, b = map(int, input().split())
+    if a > b:
+        y -= a
+    if a < b:
+        x -= b
+print(x)
+print(y)
